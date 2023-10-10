434201633fb4228f9217a2e5f66c13913b4146d0
Arina <arina@example.com>
2023-10-10 14:16:53 +0300

    сдалала вторую программу с приветствием пользователя

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Write("введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
